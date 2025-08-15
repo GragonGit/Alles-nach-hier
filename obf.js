5a128d15cc44b745d51f77aec49036063df2802a
Gragon <gragonator02@gmail.com>
2025-08-15 18:45:30 +0200

    I guess it works now

--- obf.js
+++ obf.js
@@ -1,6 +1,26 @@
-document.getElementById("showButton").addEventListener("click", myScript)
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("showButton").addEventListener("click", onImpButtonClick);
+});
 
-function myScript() {
+let isDeobf = false;
 
-  console.log("test")
+function onImpButtonClick() {
+  if (isDeobf) return;
+  
+  const elements = document.getElementsByClassName("obf");
+  Array.from(elements).forEach((el) => {
+    el.innerHTML = deobf(el.innerHTML)
+  });
+  isDeobf = true;
+  document.documentElement.style.setProperty("--obfDisplay", "block");
+}
+
+const key = "ASFFETGEF"
+
+function deobf(text) {
+  return text.split('')
+    .map((c, i) => String.fromCharCode(
+      c.codePointAt(0) ^ key.codePointAt(i % key.length))
+    )
+    .join('');
 }
